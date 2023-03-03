3eaa813cb3b01cfaff3130a937967d681edf465e
mody100000 <m.anamody10000@gmail.com>
2023-03-03 21:40:03 +0200

    translation

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import MainLayout from "../../layouts/main-layout";
+import useLocale from "../../contexts/LocaleContext";
 
 const HomePage = () => {
+  const { t } = useLocale();
   return (
     <MainLayout>
       <h1 className="text-white text-center text-3xl my-4 font-bold">
-        Home Page
+        {t("home page")}
       </h1>
     </MainLayout>
   );
